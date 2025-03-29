be43c354e2c35a671cae8e48ec4fcb26cadfd13d
poojanathirimanna <psthirimanna@gmail.com>
2025-03-29 23:48:11 +0530

    Initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+ export const signup = (req, res) => {
+    const { email, fullName, password } = req.body
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+export const login = (req, res) => {
+    res.send("Login route");
+}
+export const logout = (req, res) => {
+    res.send("Logout route");
+} 
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Connect to MongoDB BEFORE starting the server
+connectDB().then(() => {
+  app.use("/api/auth", authRoutes); // auth routes
+
+  app.listen(PORT, () => {
+    console.log('Server is running on port PORT: ' + PORT);
+  });
+});
